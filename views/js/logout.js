849390a6faa45b336c88ed2b87af2e124ca68321
Alexandra <awinterdrova@gmail.com>
2023-08-04 16:49:51 -0600

    added basic styling to the webpage

--- views/js/logout.js
+++ views/js/logout.js
@@ -6,5 +6,6 @@ fetch('http://localhost:3001/api/users/logout', options)
   .then(response => {if(response.ok){console.log("you are now logged out.")}})
   .catch(err => console.error(err));
 
+  alert("You are now logged out");
   location.href = "/";
 }
